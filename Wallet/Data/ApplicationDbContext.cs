e681cdaf905f8884560459b18c5728ffd98b100a
Borislav Yurkev <borislavurkev@gmail.com>
2022-03-03 11:56:36 +0200

    Add project files.

---
+++ Wallet/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿namespace Wallet.Data
+{
+    using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore;
+
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
